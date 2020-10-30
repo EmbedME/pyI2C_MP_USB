9e9132ccedbea80a46057070abd46a869a751a9b
EmbedME <tfischl@gmx.de>
2020-10-30 07:04:19 +0100

    Improved example for MCP4725

--- examples/mcp4725_dac.py
+++ examples/mcp4725_dac.py
@@ -11,32 +11,42 @@
 
 from i2c_mp_usb import I2C_MP_USB as SMBus
 import time
+import sys
 
-# device address 
+vcc = 3.3#Volt. DAC can be used at 2.7V to 5.5V
+# device address
 address = 0x60
+dest_dac = 0x40
+dest_eeprom = 0x20
+
 
 bus = SMBus()
 
 # Read out DAC register and EEPROM data
 rdata = bus.read_i2c_block_raw(address, 5)
-print("DAC register and EEPROM data readout:")
-print(rdata)
+print("DAC register and EEPROM data readout:",[hex(b) for b in list(rdata)])
+dac = (rdata[1]<<4) + (rdata[2]>>4)
+eeprom =(rdata[3]<<8) + rdata[4]
+volt = dac * vcc /((1<<12)-1)
+print(f"12 bit DAC at {hex(dac)} renders {volt:.2f} Volts")
+volt = eeprom * vcc /((1<<12)-1)
+print(f"12 bit EEPROM at {hex(eeprom)} renders {volt:.2f} Volts")
 
-print("Set DAC to 0V")
-bus.write_i2c_block_raw(address, [0x00, 0x00])
+print("Set DAC and EEPROM to 0V")
+bus.write_i2c_block_raw(address, [dest_dac+dest_eeprom, 0, 0, dest_dac+dest_eeprom, 0, 0])
 
 # Give some time to measure voltage on connected multimeter
 time.sleep(5)
 
 print("Set DAC to maximum")
-bus.write_i2c_block_raw(address, [0x0f, 0xff])
+bus.write_i2c_block_raw(address, [dest_dac, 0xff, 0xff, dest_dac, 0xff, 0xff])
 
 # Give some time to measure voltage on connected multimeter
 time.sleep(5)
 
-# Set any DAC value
-dacvalue = int(0xfff / 3);
-print("Set DAC value ", dacvalue)
-bus.write_i2c_block_raw(address, [(dacvalue >> 8) & 0xff, dacvalue & 0xff])
+# Set DAC value to voltage
 
-
+volt = 2.24
+dacvalue = int(min(volt/vcc,1.0) * ((1<<12)-1))
+print(f"Set DAC value {dacvalue:3x}" )
+bus.write_i2c_block_raw(address, [dest_dac, dacvalue>>4  & 0xff, dacvalue<<4  & 0xff, dest_dac, dacvalue>>4  & 0xff, dacvalue<<4  & 0xff])
